8cccc6f1e84fd7ac075441e59411b426433a5927
Emmanuel Adura <codegienuel@gmail.com>
2024-07-23 10:15:08 +0100

    integrated real movie db

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
 import { MoviesData } from "./Data/moviedata";
+import axios from "axios";
+
+const moviesDB =
+	"https://api.themoviedb.org/3/discover/movie?api_key=b2defd5282fbc07c195e2c67373704cb&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&release_date.gte=2020-01-01&release_date.lte=2023-12-31";
 
 function App() {
+	const [moviesArray, setMoviesArray] = useState([]);
+	useEffect(() => {
+		async function getMovies() {
+			try {
+				const response = await axios.get(moviesDB);
+				console.log(response);
+				console.log(response.data.results);
+				setMoviesArray(response.data.results);
+
+				return moviesArray;
+			} catch (e) {
+				console.log(e);
+			}
+		}
+
+		getMovies();
+	}, []);
+
 	return (
 		<>
 			{/* <Card
@@ -10,11 +33,11 @@ function App() {
 					Loren5 Lorem ipsum dolor sit amet. Loren5'
 			/> */}
 
-			{MoviesData.map((movie) => (
+			{moviesArray?.map((movie) => (
 				<Card
 					key={movie.id}
 					title={movie.title}
-					desc={movie.description}
+					desc={movie.overview}
 				/>
 			))}
 		</>
